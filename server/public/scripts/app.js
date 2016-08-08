6b6fef2b9d51b7ed6387f80f5a905f6a5826611c
Dave Kuntz <dkuntz811@gmail.com>
2016-08-08 09:46:48 -0500

    Monday Commit

--- server/public/scripts/app.js
+++ server/public/scripts/app.js
@@ -1,9 +1,51 @@
 $(document).ready(function(){
+  //$('#ajax-data').remove();
+  //$('#ajax-data').append(data.omicron);
+  $('#next').on('click', function() {
+
+   //console.log('This is my ' + data);
+      console.log("clicked");
+
+  });
+
+  $('#prev').on('click', function () {
+      console.log("clicked");
+  })
     $.ajax({
       type: "GET",
       url: "/data",
-      success: function(data){
-        console.log(data);
+      success: function(data){   //fetches the data from server
+
+        for (var i = 0; i < data.omicron.length; i ++) {
+          $('#ajax-data').append('<div class="omicron"></div>'); //creates a div to append data
+          var $el = $('#ajax-data').children().last();
+          $el.append('<h2>' + "Name: " + data.omicron[i].name + '</h2>');  //appends name to div
+          $el.append('<h2>' + "Git username: " + data.omicron[i].git_username+ '</h2>'); //appends git_username to div
+          $el.append('<h3>' + "Shoutout: " + data.omicron[i].shoutout + '</h3>'); //appends shoutout to div
+        }
+
+
+      console.log(data.omicron);
+
+
+      },
+      error: function () {
+        console.log('Error with request');    //displays error if data request cannot be fulfilled.
       }
+
     });
 });
+
+
+var studentArray = [];
+
+
+
+
+
+
+
+// people = omicron
+// git_username:
+// name:
+// shoutout:
